e3f0fd03c75b92aa9b0abd53fba04a259c5cd22c
HuynhLeCongLap <lapmo843@gmail.com>
2025-03-31 13:08:38 +0700

    Initial commit

---
+++ app/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class HomeController extends Controller {
+
+    public Result index() {
+        return ok("Welcome to Play Framework!");
+    }
+
+    public Result hello() {
+        return ok("Hello World");
+    }
+}
